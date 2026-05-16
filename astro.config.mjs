3dc1430b0a31d56c9462fa26fb7d346b7200cac1
Munim Kazia <munim@openpublicinfo.ie>
2026-05-16 21:31:51 +0100

    Add base path for subfolder previewing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://openpublicinfo.ie',
+  base: '/openpublicinfo',
   vite: {
     plugins: [tailwindcss()],
   },
